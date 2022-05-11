5fdef923f241de65784a1fc304f093008fa3c001
VictorSilva27 <victoradaosilva2@gmail.com>
2022-05-10 21:31:07 -0300

    Exercicios do Dia-7.1

--- Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.1/exercicios-parte-1.js
+++ Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.1/exercicios-parte-1.js
@@ -1,15 +1,15 @@
-// const teste = (escopo) =>  {
-//     if (escopo === true) {
-//         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//         ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//         console.log(ifScope);
-//     } else {
-//         let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//         console.log(elseScope);
-//     }
-// }
+const teste = (escopo) =>  {
+    if (escopo === true) {
+        let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+        console.log(ifScope);
+    } else {
+        let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+        console.log(elseScope);
+    }
+}
 
-// teste(true);
+teste(true);
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
--- Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.1/exercicios-parte-2.js
+++ Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.1/exercicios-parte-2.js
@@ -1,7 +1,41 @@
-const fatorial = x => {
-    let n = x;
-    n == 0 ? n = 1 : n * (x - 1); 
-    console.log(n);
+// Exercicio 01
+const fatorial = num => num > 1 ? num * fatorial(num - 1) : 1;
+
+console.log(fatorial(5));
+
+// Exercicio 02-
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
+
+// Exercicio 03 está no arquivo index.html
+
+// Exercicio 04
+
+const array = ["JavaScript", "HTML", "CSS", "NodeJS", "React"];
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+        `Tryber ${paramInner} aqui!
+        
+        Tudo bem?`
+        );
+        
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`;
+
+    array.forEach((skill) =>
+    result = `${result}
+
+    - ${skill}`);
+
+    result = `
+${result}
+
+    #VQV
+    `;
+
+    return result;
 }
 
-fatorial(4);
+console.log(buildSkillsPhrase("Victor"));
