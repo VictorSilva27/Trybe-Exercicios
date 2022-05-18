2c330b9c639d662dd04191d93eafd815fa4fd7f0
VictorSilva27 <victoradaosilva2@gmail.com>
2022-05-18 15:14:55 -0300

    Adicionando Conteudo e Exercicios para fixar, do Dia-8.4

---
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/Dia-8.4/paraFicar.js
@@ -0,0 +1,100 @@
+// // Para Fixar Parte 01
+
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const pairFull = (currentValue, number) => (
+//   (number % 2 === 0) ? currentValue + number : currentValue
+// );
+
+// const sum = (array) => array.reduce(pairFull, 100);
+
+// console.log(sum(numbers));
+
+// Para Fixar Parte 02
+
+const estudantes = [
+    {
+        nome: 'Jorge',
+        sobrenome: 'Silva',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 67 },
+            { name: 'Português', nota: 79 },
+            { name: 'Química', nota: 70 },
+            { name: 'Biologia', nota: 65 },
+        ],
+    },
+    {
+        nome: 'Mario',
+        sobrenome: 'Ferreira',
+        idade: 15,
+        turno: 'Tarde',
+        materias: [
+            { name: 'Matemática', nota: 59 },
+            { name: 'Português', nota: 80 },
+            { name: 'Química', nota: 78 },
+            { name: 'Biologia', nota: 92 },
+        ],
+    },
+    {
+        nome: 'Jorge',
+        sobrenome: 'Santos',
+        idade: 15,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 76 },
+            { name: 'Português', nota: 90 },
+            { name: 'Química', nota: 70 },
+            { name: 'Biologia', nota: 80 },
+        ],
+    },
+    {
+        nome: 'Maria',
+        sobrenome: 'Silveira',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 91 },
+            { name: 'Português', nota: 85 },
+            { name: 'Química', nota: 92 },
+            { name: 'Biologia', nota: 90 },
+        ],
+    },
+    {
+        nome: 'Natalia',
+        sobrenome: 'Castro',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 70 },
+            { name: 'Português', nota: 70 },
+            { name: 'Química', nota: 60 },
+            { name: 'Biologia', nota: 50 },
+        ],
+    },
+    {
+        nome: 'Wilson',
+        sobrenome: 'Martins',
+        idade: 14,
+        turno: 'Manhã',
+        materias: [
+            { name: 'Matemática', nota: 80 },
+            { name: 'Português', nota: 82 },
+            { name: 'Química', nota: 79 },
+            { name: 'Biologia', nota: 75 },
+        ],
+    },
+];
+
+const maiorNota = (acc, materia) => {
+    if (acc.nota > materia.nota) return acc;
+    return materia;
+}
+
+const melhorMateria = (alunos) => alunos.map((aluno) => ({
+    name: aluno.nome,
+    materia: aluno.materias.reduce(maiorNota).name
+}));
+
+console.log(melhorMateria(estudantes));
