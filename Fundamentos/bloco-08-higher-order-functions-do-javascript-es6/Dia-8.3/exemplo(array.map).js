75b684bb97c0332f19e480926f98ae982eeb1532
VictorSilva27 <victoradaosilva2@gmail.com>
2022-05-17 21:12:35 -0300

    Exercicios do 01 ao 06

--- Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/Dia-8.3/exemplo(array.map).js
+++ Fundamentos/bloco-08-higher-order-functions-do-javascript-es6/Dia-8.3/exemplo(array.map).js
@@ -1,36 +1,36 @@
-// const persons = [
-//     { firstName: 'Maria', lastName: 'Ferreira' },
-//     { firstName: 'João', lastName: 'Silva' },
-//     { firstName: 'Antonio', lastName: 'Cabral' },
-//   ];
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
 
-//   const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
 
-//   console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
 
-// const numbers = [1, 2, 3, 4, -5];
+const numbers = [1, 2, 3, 4, -5];
 
-// const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
 
-// console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
-// console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
 
-// const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
 
-// const prices = [2.99, 3.99, 1.5, 2];
+const prices = [2.99, 3.99, 1.5, 2];
 
-// const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
-//   { [product]: listPrices[index] }
-// ));
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
 
-// const listProducts = updateProducts(products, prices);
-// console.log(listProducts);
-// // => [
-// //   { Arroz: 2.99 },
-// //   { Feijao: 3.99 },
-// //   { Alface: 1.5 },
-// //   { Tomate: 2 },
-// // ]
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
 
 const estudantes = [
     {
