a3d69c50180d4121c9dc5a1802f7dbeb935f86a1
VictorSilva27 <victoradaosilva2@gmail.com>
2022-05-29 13:25:24 -0300

    Exercicios Bônus 1 e 2 (Parte 03)

--- Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.2/Parte-03/script.js
+++ Fundamentos/bloco-07-introdução-à-JavaScript-ES6-e-testes-unitários/Dia-7.2/Parte-03/script.js
@@ -83,18 +83,18 @@ const getInfo = (obj, name) => {
     let allStudent = 0;
     const array = Object.values(obj);
     for (index in array) {
-      if (array[index].professor === name) {
-        allLessons.push(array[index].materia)
-        allStudent += array[index].numeroEstudantes;
-      }
+        if (array[index].professor === name) {
+            allLessons.push(array[index].materia)
+            allStudent += array[index].numeroEstudantes;
+        }
     }
     return { lessons: allLessons, estudantes: allStudent };
-  }
-  
-  const createReport = (allLessons, name) => {
+}
+
+const createReport = (allLessons, name) => {
     const report = {};
     report.professor = name;
     Object.assign(report, getInfo(allLessons, name));
     return report;
-  }
-  console.log(createReport(allLessons, 'Maria Clara'));
+}
+console.log(createReport(allLessons, 'Maria Clara'));
