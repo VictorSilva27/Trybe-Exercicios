6363af2b09a2d86c19ae0ad1ee4d4b2121816a0b
VictorSilva27 <victoradaosilva2@gmail.com>
2022-05-31 19:21:39 -0300

    exercicio-05.js 2.0

--- Frond-End/bloco-9-javascript-e-testes-assincronos/Dia-9.1/exercicios.js
+++ Frond-End/bloco-9-javascript-e-testes-assincronos/Dia-9.1/exercicios.js
@@ -1,32 +1,3 @@
-// Exercicio 03
-// const getPlanet = () => {
-//     const mars = {
-//         name: "Mars",
-//         distanceFromSun: {
-//             value: 227900000,
-//             measurementUnit: "kilometers",
-//         },
-//     };
-//     console.log("Returned planet: ", mars);
-// };
-// setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
-
-// Exercicio 04
-// const messageDelay = () => Math.floor(Math.random() * 5000);
-
-// const getMarsTemperature = () => {
-//     const maxTemperature = 58;
-//     return Math.floor(Math.random() * maxTemperature);
-// };
-// // crie a função sendMarsTemperature abaixo
-// const sendMarsTemperature = (callback) => {
-//     console.log(`Mars temperature is ${callback} degreee Celsius`);
-// }
-
-// setTimeout(() => {
-//     sendMarsTemperature(getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo 
-// }, messageDelay());
-
 // Exercicio 05
 const messageDelay = () => Math.floor(Math.random() * 5000);
 
