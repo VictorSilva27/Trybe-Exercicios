164913dc7c2524169303bfac659fb80cb8909830
VictorSilva27 <victoradaosilva2@gmail.com>
2022-06-01 20:03:00 -0300

    Arrumando CSS

--- Frond-End/bloco-9-javascript-e-testes-assincronos/Dia-9.2/exercicios-02-ao-05/api.js
+++ Frond-End/bloco-9-javascript-e-testes-assincronos/Dia-9.2/exercicios-02-ao-05/api.js
@@ -9,17 +9,41 @@ const fetchCoins = async () => {
     return coins;
 }
 
-const setCoins = async () => {
-    const coins = await fetchCoins();
-    const coinsList = document.getElementById('coins-list');
+const fetchUsdCurrencies = async () => {
+    const baseUrl = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest'
+    const usdEndpoint = '/currencies/usd.min.json'
+    const url = baseUrl.concat(usdEndpoint);
 
+    const usdCurrencies = await fetch(url)
+        .then((response) => response.json())
+        .then((data) => data.usd)
+        .catch((error) => error.toString());
+
+    return usdCurrencies;
+};
+
+const setCoinsBrl = async () => {
+    const coins = await fetchCoins();
+    const usdCurrencies = await fetchUsdCurrencies();
+    const coinsList = document.getElementById('coins-list2');
+    coins
+        .filter((coin) => Number(coin.rank) <= 10)
+        .forEach((coin) => {
+            const newLi = document.createElement('li');
+            const usdPrice = Number(coin.priceUsd);
+            const brlPrice = usdPrice * usdCurrencies.brl;
+
+            newLi.innerText = `${coin.name} (${coin.symbol}): ${brlPrice.toFixed(2)}`;
+
+            coinsList.appendChild(newLi);
+        });
+    const coinsListUsd = document.getElementById('coins-list');
     coins
         .filter((coin) => Number(coin.rank) <= 10)
         .forEach((coin) => {
             const newLi = document.createElement('li');
             newLi.innerText = `${coin.name} (${coin.symbol}): ${coin.priceUsd}`;
-            coinsList.appendChild(newLi);
+            coinsListUsd.appendChild(newLi);
         });
-}
-
-window.onload = setCoins;
+};
+window.onload = setCoinsBrl;
