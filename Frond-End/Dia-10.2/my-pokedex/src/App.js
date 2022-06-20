ac44f54ad76a51e0f67a81ca15f06303789783c9
VictorSilva27 <victoradaosilva2@gmail.com>
2022-06-20 18:59:42 -0300

    My-Pokedex Estilizado

--- Frond-End/Dia-10.2/my-pokedex/src/App.js
+++ Frond-End/Dia-10.2/my-pokedex/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-//Import do json criado com os pokemons e seus dados.
 import Pokedex from './Pokedex';
 
 class App extends React.Component {
